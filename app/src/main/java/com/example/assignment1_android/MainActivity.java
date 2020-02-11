4ed7274a9971b4b75e8d3f4d889cc4baf1a0a20c
Ankita Jain <ankita.jain12342@gmail.com>
2020-02-10 19:46:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/assignment1_android/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.assignment1_android;
+
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    double  latitude,longitude,altitude,accuracy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestPermission();
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        Button getLocationBtn = findViewById(R.id.get_Location_Btn);
+
+        getLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                    requestPermission();
+                    return;
+                }
+
+                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location!=null)
+                        {
+                            TextView lat = findViewById(R.id.lat);
+                            TextView longi = findViewById(R.id.longitude);
+                            TextView alt = findViewById(R.id.altitude);
+                            TextView acc = findViewById(R.id.accuracy);
+                            TextView add = findViewById(R.id.address);
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            altitude = location.getAltitude();
+                            accuracy = location.getAccuracy();
+                            String address = getAddress(latitude,longitude);
+
+
+                            lat.setText(String.valueOf(latitude));
+                            longi.setText(String.valueOf(longitude));
+                            alt.setText(String.valueOf(altitude));
+                            acc.setText(String.valueOf(accuracy));
+                            add.setText(address);
+
+
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void requestPermission()
+    {
+        ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION},1);
+
+    }
+
+    private String getAddress(double latitude, double longitude) {
+        StringBuilder result = new StringBuilder();
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses.size() > 0) {
+                Address address = addresses.get(0);
+                result.append(address.getLocality()).append("\n");
+                result.append(address.getCountryName());
+            }
+        } catch (IOException e) {
+            Log.e("tag", e.getMessage());
+        }
+
+        return result.toString();
+    }
+
+}
+
+
